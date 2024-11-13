22b31c57898956fc752c29c6172c808dc927f923
martin-tercero1 <martintercero20@hotmail.com>
2024-11-13 13:56:42 -0600

    Integracion de la seccion de Testimonios

--- src/components/home/Contact/Contact.jsx
+++ src/components/home/Contact/Contact.jsx
@@ -9,19 +9,19 @@ export const Contact = () => {
         <h2 className="text-xl font-bold ">¡Contactate con nosotros!</h2>
       </div>
       <div className="flex flex-col items-center gap-[24px]">
-        <span className="text-md text-grey ">
+        <span className="text-md font-bold text-grey text-center">
           Llená el formulario y envianos tu contacto para que podamos
           comunicarnos con vos.
         </span>
         <Button
           size="large"
           variant="primary"
-          customStyles={["w-auto", "underline"]}
+          customStyles={["w-auto", "underline", "font-bold"]}
           text="Ir a formulario de adopciones"
           link="https://goo.gl/forms/vRAFc3yV8MWCZ1yJ2"
         ></Button>
         <div className="flex flex-col items-center">
-          <p className="text-md text-grey">O escribinos a:</p>
+          <p className="text-md text-grey font-semibold">O escribinos a:</p>
           <a
             className="text-md text-[#0082D8]"
             href="mailto:info@soplodevida.org"
--- src/components/home/Testimonials/Testimonials.jsx
+++ src/components/home/Testimonials/Testimonials.jsx
@@ -19,20 +19,20 @@ export const Testimonials = () => {
   };
 
   return (
-    <div className="w-auto h-[743px] flex flex-col items-center bg-white relative">
-      <h1 className="font-bold text-black text-3xl mb-8 mt-4">Testimonios</h1>
+    <section id="testimonios" className="w-auto h-[743px] flex flex-col items-center bg-white relative">
+      <h1 className="font-bold text-black text-xl text-center mb-7 mt-1">Testimonios</h1>
 
-      <div className="overflow-hidden w-full relative flex justify-center">
+      <div className="overflow-hidden w-full relative flex justify-center pl-8">
         <motion.div
           ref={scrollRef}
-          className={`flex space-x-7 overflow-hidden ${
+          className={`flex space-x-3 overflow-hidden ${
             testimonios.length > 5 ? "md:pl-20 md:pr-10" : "md:ml-0 md:mr-0"
           }`}
         >
           {testimonios.map((testimonio, index) => (
             <div
               key={index}
-              className="w-[326px] h-[570px] p-4 flex-shrink-0"
+              className="w-[326px] h-[570px] p-1 pb-4 flex-shrink-0"
               style={{ scrollSnapAlign: "start" }}
             >
               <Image
@@ -44,7 +44,7 @@ export const Testimonials = () => {
                   testimonio.border ? testimonio.border : ""
                 }`}
               />
-              <h2 className="font-medium text-[#404552] text-center text-2xl">
+              <h2 className="font-medium text-[#404552] text-center text-md">
                 {testimonio.name}
               </h2>
               <p className="text-gray-600 font-normal text-center mt-4 text-base">
@@ -58,29 +58,29 @@ export const Testimonials = () => {
       {flag && (
         <button
           onClick={handlePrev}
-          className="absolute h-[64px] w-[64px] left-5 top-1/3 transform -translate-y-1/2 bg-[#FADBC7] p-2 rounded-full"
+          className="absolute h-[64px] w-[64px] left-5 top-1/3 transform bg-secondary/50 p-2 rounded-full flex items-center justify-center"
         >
           <Image
             src={arrow}
             alt={`Next arrow`}
             height={20}
             width={20}
-            className="w-[16.74px] h-[35.56px] rotate-180 ml-4"
+            className="w-[16.74px] h-[35.56px] rotate-180"
           />
         </button>
       )}
       <button
         onClick={handleNext}
-        className="absolute h-[64px] w-[64px] right-5 top-1/3 transform -translate-y-1/2 bg-[#FADBC7] p-2 rounded-[35px]"
+        className="absolute h-[64px] w-[64px] right-5 top-1/3 transform bg-secondary/50 p-2 rounded-[35px] flex items-center justify-center"
       >
         <Image
           src={arrow}
           alt={`Next arrow`}
           height={20}
           width={20}
-          className="w-[16.74px] h-[35.56px] ml-4"
+          className="w-[16.74px] h-[35.56px]"
         />
       </button>
-    </div>
+    </section>
   );
 };
