f72db0d6c8bdfa2fe0e95ed8dae841f1cdad3181
martin-tercero1 <martintercero20@hotmail.com>
2024-11-12 20:42:53 -0600

    Ajustes necesarios para integrar seccion a develop
    Actualizar imagen al correcto ancho y altura original
    Añadir paleta de colores, espaciado, tipo de fuente y tamaño de fuente
    Reutilizar el componente boton
    Actualizar los espaciados y margenes siguiendo el archivo tailwind config

--- src/components/home/About/About.jsx
+++ src/components/home/About/About.jsx
@@ -1,109 +1,91 @@
+import { Button } from "@/components/shared/Button";
+
 export const About = () => {
   return (
     <>
       <section className="p-8 bg-[#FAFAFA]">
-        <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-6">
+        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-3">
           <div className="text-center">
-            <div className="bg-[#FADBC7] p-4 rounded-md">
+            <div className="bg-[#FADBC7] px-7 py-6 rounded-xl">
               <img
-                className="mx-auto mt-6"
+                className="mx-auto"
                 src="/hiking.png"
                 alt="imagen de referencia"
               />
-              <svg
-                className="mx-auto w-12 h-10"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              ></svg>
             </div>
-            <h3 className="font-bold mt-5 text-[#404552] text-[18px]">
+            <h3 className="font-bold mt-2.5 text-[#404552] text-[18px]">
               Misión
             </h3>
-            <p className="mt-4 text-[#404552] text-base">
+            <p className="mt-2 text-[#404552] text-base">
               Ser el nexo entre el pasado y el futuro de perros y gatos sin
               hogar.  Encontrar el HOGAR para todos los animales que buscan
               crecer felices. .
             </p>
-            <p className="mt-4 text-[#404552]">
+            <p className="mt-2 font-semibold text-[#404552]">
               ¡Nuestros &apos;soplitos&apos; cuentan con ustedes!
             </p>
           </div>
           <div className="text-center">
-            <div className="bg-[#D6EDF0] p-4 rounded-md">
+            <div className="bg-[#D6EDF0] px-7 py-6 rounded-xl">
               <img
-                className="mx-auto mt-6"
+                className="mx-auto"
                 src="/public.png"
                 alt="imagen de referencia"
               />
-              <svg
-                className="mx-auto w-12 h-10"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              ></svg>
             </div>
-            <h3 className="font-bold mt-5 text-[#404552] text-[18px]">
+            <h3 className="font-bold mt-2.5 text-[#404552] text-[18px]">
               Visión
             </h3>
-            <p className="mt-4 text-[#404552] text-base">
+            <p className="mt-2 text-[#404552] text-base">
               Concientizar con la ACCIÓN para que todos trabajemos juntos
               intentando un mundo más justo para los más vulnerables.
             </p>
-            <p className="mt-4 text-[#404552]">¡Juntos podemos más!</p>
+            <p className="mt-2 font-semibold text-[#404552]">¡Juntos podemos más!</p>
           </div>
           <div className="text-center">
-            <div className="bg-[#FAF5E3] p-4 rounded-md">
+            <div className="bg-[#FAF5E3] px-7 py-6 rounded-xl">
               <img
-                className="mx-auto mt-[52px]"
+                className="mx-auto"
                 src="/Vector.png"
                 alt="imagen de referencia"
               />
-              <svg
-                className="mx-auto w-12 h-10"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              ></svg>
             </div>
-            <h3 className="font-bold mt-5 text-[#404552] text-[18px]">
+            <h3 className="font-bold mt-2.5 text-[#404552] text-[18px]">
               Valores
             </h3>
-            <p className="mt-4 text-[#404552] text-base">
+            <p className="mt-2 text-[#404552] text-base">
               La palabra HOGAR es sinónimo de seguridad, de calma. Todos
               merecemos un hogar para no pasar frío, hambre y miedo.
             </p>
-            <p className="mt-4 text-[#404552]">
+            <p className="mt-2 font-semibold text-[#404552]">
               ¡Todos los ‘soplitos’ merecen un hogar!
             </p>
           </div>
 
           <div className="text-center">
-            <div className="bg-[#E3DBEB] p-4 rounded-md">
+            <div className="bg-[#E3DBEB] px-7 py-6 rounded-xl">
               <img
-                className="mx-auto mt-6"
+                className="mx-auto"
                 src="/pets.png"
                 alt="imagen de referencia"
               />
-              <svg
-                className="mx-auto w-12 h-10"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              ></svg>
             </div>
-            <h3 className="font-bold mt-5 text-[#404552] text-[18px]">
+            <h3 className="font-bold mt-2.5 text-[#404552] text-[18px]">
               Adopciones
             </h3>
-            <p className="mt-4 text-[#404552] text-base">
+            <p className="mt-2 text-[#404552] text-base">
               Adoptar es dar otra oportunidad, amar incondicionalmente y crecer
               con otro. Por eso, es nuestro pilar e incentivamos la adopción.
             </p>
-            <p className="mt-4 text-[#404552]">¡Animate a adoptar!</p>
+            <p className="mt-2 font-semibold text-[#404552]">¡Animate a adoptar!</p>
           </div>
         </div>
       </section>
       {/* <!--sección principal con imagen y descripción--> */}
-      <section className="py-12 bg-[#FAF5E3]">
+      <section className="px-8 py-5 bg-[#FAF5E3]">
         <div className="max-w-7xl mx-auto flex flex-col md:flex-row gap-6">
-          <div className="w-full md:w-1/2 mb-6 md:mb-0">
-            <div className="rounded-lg overflow-hidden p-4">
+          <div className="w-full md:w-1/2  md:mb-0">
+            <div className="rounded-lg overflow-hidden w-[515px] h-[557px]">
               <img
                 className="mx-auto justify-center object-cover w-full h-full rounded-lg"
                 src="/Vector 6.png"
@@ -111,11 +93,11 @@ export const About = () => {
               />
             </div>
           </div>
-          <div className="w-full md:w-1/2 text-center md:text-left md:pl-8">
-            <h2 className="text-[48px] text-[#404552] ml-2 mt-6">
+          <div className="w-full md:w-1/2 text-center md:text-left md:pl-4 self-center">
+            <h2 className="text-[48px] font-medium text-[#404552] ml-2">
               Soplo de Vida
             </h2>
-            <p className="mb-4 text-[#404552] ml-2 pr-6 mt-6 text-base">
+            <p className="mb-4 text-[#404552] ml-2 mr-3 mt-3 text-base">
               Soplo de Vida es una fundación de animales que al momento alberga
               más de 120 perros y gatos rescatados en pésimas condiciones. Todos
               ellos esperan ser adoptados por una familia. Además de este
@@ -129,27 +111,29 @@ export const About = () => {
               instantáneamente a una atmósfera cálida repleta de respeto, paz y
               amor.
             </p>
-            <div className="flex justify-center md:justify-start gap-4">
-              <a
+            <div className="flex justify-center md:justify-start gap-4 ml-2">
+              <Button variant="primary" size="large" text="Donar" />
+              {/* <a
                 href="#"
                 className=" text-[#404552] text-[24px] w-[209px] h-[65px] text-center pt-3 ml-2 mt-12 bg-transparent border border-[#50276A] text-black px-4 py-2 rounded-md hover:bg-[#E3DBEB] hover:text-black
            "
               >
                 Ser voluntario
-              </a>
-              <a
+              </a> */}
+              <Button variant="secondary" size="large" text="Ser voluntario" />
+              {/* <a
                 href="#"
                 className="text-[#404552] text-[24px] w-[209px] h-[65px] text-center pt-3 mt-12 bg-[#50276A] text-white px-4 py-2 rounded-md hover:bg-[#E3DBEB] hover:text-black border border-[#50276A]
            "
               >
                 Donar
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
       </section>
       {/* <!--sección de estadísticas--> */}
-      <section className="py-8 bg-white">
+      <section className="py-6.5 bg-white">
         <div className="max-w-3xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 ">
           <div className="flex justify-center">
             <img
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,43 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      primary: "#50276A",
+      secondary: "#E3DBEB",
+      white: "#FAFAFA",
+      grey: "#404552",
+      black: "#0F0F0F",
+      orange: "#FADBC7",
+      "dark-orange": "#C77A78",
+      "light-green": "#D1EDE6",
+      "dark-green": "#78B5A6",
+      "light-yellow": "#FAF5E3",
+      "dark-yellow": "#C99470",
+      violet: "#756685",
+    },
+    fontFamily: {
+      sans: ["Nunito", "sans"],
+    },
+    spacing: {
+      1: "14px",
+      2: "16px",
+      2.5: "18px",
+      3: "24px",
+      4: "32px",
+      5: "48px",
+      6: "56px",
+      6.5: "64px",
+      7: "80px",
+      8: "88px"
+    },
+    fontSize: {
+      sm: "14px",
+      base: "16px",
+      md: "24px",
+      xl: "32px",
+      '2xl': "48px",
+    },
     extend: {
       colors: {
         background: "var(--background)",
