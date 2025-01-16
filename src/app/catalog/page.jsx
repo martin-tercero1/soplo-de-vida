030aefb43959832e2372a36437304030c2c4a698
JulianAgPerez <juli.agustin.perez@gmail.com>
2025-01-16 19:51:35 -0300

    ft: added filter.svg icon

--- src/app/catalog/page.jsx
+++ src/app/catalog/page.jsx
@@ -1,11 +1,11 @@
 "use client"
 
 import AnimalCards from '@/components/catalog/Card/AnimalCards';
-import { Card } from '@/components/catalog/Card/Card'
 import { Header } from "@/components/home/Header";
 import React, {useState} from 'react'
 import Image from 'next/image';
 import backIcon from '/public/icons/back.svg';
+import filterIcon from '/public/icons/filter.svg';
 
 export default function Catalog () {
   const [isPopUpVisible, setIsPopUpVisible] = useState(false);
@@ -22,14 +22,21 @@ export default function Catalog () {
           <Image src={backIcon} alt="Volver" className="mr-[9px] " />
           Volver
         </button>
-      <h2 className='py-2 font-bold text-[18px]'>Nuestros soplitos</h2>
-      <div className="mb-[8px]">
-        <input
-        type='text'
-        placeholder='Buscar'
-        className='px-2 border border-grey rounded-[10px] w-[95%] '>
-        </input>
-      </div>
+        <h2 className='py-2 font-bold text-[18px]'>Nuestros soplitos</h2>
+      {/**Search-bar*/}
+        <div className="mb-[8px]">
+          <input
+          type='text'
+          placeholder='Buscar'
+          className='px-2 border border-grey rounded-[10px] w-[95%] '>
+          </input>
+        </div>
+        {/**Filtros*/}
+        <button className='flex py-[13.5] font-bold text-[14px]'>
+          <Image src={filterIcon} alt="Filtros" className="mr-[9px]"/>
+          Filtros
+        </button>
+
         <AnimalCards/>
       </div>
     </>
