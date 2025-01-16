fd13b066402bcb8fd1dd5e1a70d22d7cb0062591
MarianoBaeza <marianobaeza@hotmail.com>
2025-01-16 16:38:03 -0300

    adds Filtering, Sorting, Pagination handling, Error handling + improvements

--- src/app/api/pets/[id]/route.ts
+++ src/app/api/pets/[id]/route.ts
@@ -1,58 +1,97 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../utils/prisma";
-;
+import type { Pet } from "../../types";
 
-export async function GET(req: NextRequest, { params }: any) {
+type Params = {
+  params: {
+    id: string;
+  };
+};
+
+type PetUpdate = {
+  name?: string;
+  type?: string;
+  age?: string;
+  gender?: string;
+  personality?: string;
+  size?: string;
+  description?: string;
+  health_condition?: string;
+  castrated?: boolean;
+  dewormed?: boolean;
+  images?: string[];
+  urgency?: boolean;
+};
+
+export async function GET({ params }: Params) {
+  try {
     const { id } = await params;
 
-    const pet = await prisma.pet.findUnique({
-        where: {
-            id: id?.toString(),
-        },
-    })
+    const pet: Pet | null = await prisma.pet.findUnique({
+      where: {
+        id: id?.toString(),
+      },
+    });
+
+    if (!pet) {
+      return NextResponse.json({ error: "Pet not found" }, { status: 404 });
+    }
+
     return NextResponse.json({
-        pet
-    })
+      data: pet,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "An error occurred while fetching the pet" },
+      { status: 500 }
+    );
+  }
 }
 
-export async function PATCH(req: NextRequest, { params }: any) {
+export async function PATCH(req: NextRequest, { params }: Params) {
+  try {
     const { id } = await params;
-    console.log(params)
-    const { name, type, age, gender, personality, size, description, health_condition, castrated, dewormed, images } = await req.json();
 
-    const pet = await prisma.pet.update({
-        where: {
-            id: id,
-        },
-        data: {
-            name,
-            type,
-            age,
-            gender,
-            personality,
-            size,
-            description,
-            health_condition,
-            castrated,
-            dewormed,
-            images
-        }
-    })
+    const {
+      name,
+      type,
+      age,
+      gender,
+      personality,
+      size,
+      description,
+      health_condition,
+      castrated,
+      dewormed,
+      images,
+    } = (await req.json()) as PetUpdate;
+
+    const pet: Pet = await prisma.pet.update({
+      where: {
+        id: id,
+      },
+      data: {
+        name,
+        type,
+        age,
+        gender,
+        personality,
+        size,
+        description,
+        health_condition,
+        castrated,
+        dewormed,
+        images,
+      },
+    });
 
     return NextResponse.json({
-        pet
-    })
-}
-
-export async function DELETE(req: NextRequest) {
-    const id = req.nextUrl.searchParams.get('id') as string;
-
-    await prisma.pet.delete({
-        where: {
-            id: id,
-        },
-    })
-    return NextResponse.json({
-        data: `Pet with id: ${id} has been deleted`
-    })
+      data: pet,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "An error occurred while updating the pet" },
+      { status: 500 }
+    );
+  }
 }
--- src/app/api/pets/route.ts
+++ src/app/api/pets/route.ts
@@ -1,34 +1,145 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../utils/prisma";
-import { NextApiRequest } from "next";
+import type { Pet } from "../types";
 
-export async function GET() {
-    const pets = await prisma.pet.findMany()
+type PetsFilterProps = {
+  type?: string;
+  gender?: string;
+  size?: string;
+  castrated?: boolean;
+  sortBy?: string;
+  order?: string;
+  page?: number;
+  limit?: number;
+};
+
+type GetPetsResponse = {
+  pets: any[];
+  page: number;
+  limit: number;
+  totalResults: number;
+  petCount: number;
+};
+
+type CreatePet = {
+  name: string;
+  type: string;
+  age: string;
+  gender: string;
+  personality: string;
+  size?: string;
+  description?: string;
+  health_condition?: string;
+  castrated: boolean;
+  dewormed: boolean;
+  images: string[];
+  urgency?: boolean;
+};
+
+export async function GET(req: NextRequest) {
+  try {
+    const {
+      type,
+      gender,
+      size,
+      castrated,
+      sortBy = "name",
+      order = "asc",
+      page = 1,
+      limit = 10,
+    } = Object.fromEntries(req.nextUrl.searchParams) as PetsFilterProps;
+
+    const pets = await prisma.pet.findMany({
+      where: {
+        type,
+        gender,
+        size,
+        castrated,
+      },
+      orderBy: [
+        {
+          [sortBy]: order,
+        },
+      ],
+      skip: (page - 1) * limit,
+      take: limit * 1,
+    });
+
+    const totalResults = await prisma.pet.count({
+      where: {
+        type,
+        gender,
+        size,
+        castrated,
+      },
+    });
+
+    const result: GetPetsResponse = {
+      pets,
+      page,
+      limit,
+      totalResults,
+      petCount: pets.length,
+    };
+
     return NextResponse.json({
-        pets
-    })
+      data: result,
+    });
+  } catch (error) {
+    console.error("Error fetching pets:", error);
+    return NextResponse.json(
+      {
+        error: "An error occurred while fetching pets.",
+      },
+      { status: 500 }
+    );
+  }
 }
 
 export async function POST(req: NextRequest) {
-    const { name, type, age, gender, personality, size, description, health_condition, castrated, dewormed, images } = await req.json();
+  try {
+    const {
+      name,
+      type,
+      age,
+      gender,
+      personality,
+      size,
+      description,
+      health_condition,
+      castrated,
+      dewormed,
+      images,
+      urgency,
+    } = (await req.json()) as CreatePet;
 
-    const pet = await prisma.pet.create({
-        data: {
-            name,
-            type,
-            age,
-            gender,
-            personality,
-            size,
-            description,
-            health_condition,
-            castrated,
-            dewormed,
-            images
-        }
+    const pet: Pet = await prisma.pet.create({
+      data: {
+        name,
+        type,
+        age,
+        gender,
+        personality,
+        size,
+        description,
+        health_condition,
+        castrated,
+        dewormed,
+        images,
+        urgency,
+      },
     });
 
     return NextResponse.json({
-        pet
-    })
+      data: pet,
+    });
+  } catch (error) {
+    console.error("Error creating pet:", error);
+    return NextResponse.json(
+      {
+        error: "An error occurred while creating the pet.",
+      },
+      { status: 500 }
+    );
+  }
 }
