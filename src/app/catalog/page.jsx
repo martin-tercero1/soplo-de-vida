e37944d36a93433223495d675e2d1f7477299325
JulianAgPerez <juli.agustin.perez@gmail.com>
2025-01-16 16:39:46 -0300

    ft: added back button in catalog

--- src/app/catalog/page.jsx
+++ src/app/catalog/page.jsx
@@ -4,6 +4,8 @@ import AnimalCards from '@/components/catalog/Card/AnimalCards';
 import { Card } from '@/components/catalog/Card/Card'
 import { Header } from "@/components/home/Header";
 import React, {useState} from 'react'
+import Image from 'next/image';
+import backIcon from '/public/icons/back.svg';
 
 export default function Catalog () {
   const [isPopUpVisible, setIsPopUpVisible] = useState(false);
@@ -15,7 +17,12 @@ export default function Catalog () {
   return (
     <>
       <Header togglePopUp={togglePopUp} />
-      <div className='p-2'>
+      <div className='p-2 font-sans'>
+        <button className="flex sm:hidden ">
+          <Image src={backIcon} alt="Volver" className="mr-[9px] " />
+          Volver
+        </button>
+      <h2 className='pt-2 pb-2 font-bold text-[18px]'>Nuestros soplitos</h2>
         <AnimalCards/>
       </div>
     </>
