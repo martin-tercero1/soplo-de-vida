1a639f71942e80c28e29c3e918370e75fcd7594f
JulianAgPerez <juli.agustin.perez@gmail.com>
2025-01-16 15:51:04 -0300

    ft: added component "AnimalCards"
    encapsulates the functionality of displaying cards

--- src/app/catalog/page.jsx
+++ src/app/catalog/page.jsx
@@ -1,26 +1,23 @@
-import { Card } from '@/components/catalog/Card/Card'
-import React from 'react'
+"use client"
 
-//mapear un array de animales para completar las cards por ej
+import AnimalCards from '@/components/catalog/Card/AnimalCards';
+import { Card } from '@/components/catalog/Card/Card'
+import { Header } from "@/components/home/Header";
+import React, {useState} from 'react'
 
 export default function Catalog () {
-    const animals = [
-        { image: 'https://placehold.co/600x400', name: 'Gato', age: 1 },
-        { image: 'https://placehold.co/600x400', name: 'Perro', age: 2 },
-        { image: 'https://placehold.co/600x400', name: 'Conejo', age: 3 },
-        { image: 'https://placehold.co/600x400', name: 'Hamster', age: 1 },
-    ];
+  const [isPopUpVisible, setIsPopUpVisible] = useState(false);
+
+  const togglePopUp = () => {
+    setIsPopUpVisible((prevValue) => !prevValue);
+  };
 
   return (
-    <div className="p-2 grid grid-cols-2 grid-rows-4 gap-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4">
-    {animals.map((animal, index) => (
-      <Card
-        key={index}
-        image={animal.image}
-        name={animal.name}
-        age={animal.age}
-      />
-    ))}
-  </div>
+    <>
+      <Header togglePopUp={togglePopUp} />
+      <div className='p-2'>
+        <AnimalCards/>
+      </div>
+    </>
   )
 }
---
+++ src/components/catalog/Card/AnimalCards.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Card } from '@/components/catalog/Card/Card';
+
+const AnimalCards = () => {
+
+    //Hardodeado, eliminar cuando conecte a API
+    const animals = [
+            { image: 'https://placehold.co/600x400', name: 'Gato', age: 1 },
+            { image: 'https://placehold.co/600x400', name: 'Perro', age: 2 },
+            { image: 'https://placehold.co/600x400', name: 'Conejo', age: 3 },
+            { image: 'https://placehold.co/600x400', name: 'Hamster', age: 1 },
+    ];
+    
+  return (
+    <div id='cards-section' className="grid grid-cols-2 grid-rows-4 gap-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4">
+      {animals.map((animal, index) => (
+        <Card
+          key={index}
+          image={animal.image}
+          name={animal.name}
+          age={animal.age}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default AnimalCards;
