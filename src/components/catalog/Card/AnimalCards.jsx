7999f22feec774424c307e7c5a3d6ab296d48acf
JulianAgPerez <juli.agustin.perez@gmail.com>
2025-01-23 18:36:38 -0300

    refactor: added padding into searchbar

--- src/components/catalog/Card/AnimalCards.jsx
+++ src/components/catalog/Card/AnimalCards.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card } from '@/components/catalog/Card/Card';
 
 const AnimalCards = () => {
+  /*
   const { animals, loading, error } = useAnimals();
 
   //Cambiar por componente loader cuando haya (o lo cree)
@@ -14,7 +15,7 @@ const AnimalCards = () => {
     return <div>Error: {error.message}</div>;
   }
 
-/*
+*/
     //Hardodeado, eliminar cuando conecte a API
     const animals = [
             { image: 'https://placehold.co/600x400', name: 'Gato', age: 1 },
@@ -22,7 +23,7 @@ const AnimalCards = () => {
             { image: 'https://placehold.co/600x400', name: 'Gato jorge', age: 3 },
             { image: 'https://placehold.co/600x400', name: 'Perro pepe', age: 1 },
     ];
-  */  
+  
  
   return (
     <div id='cards-section' className="grid grid-cols-2 grid-rows-4 gap-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4">
--- src/components/shared/Search-bar/SearchBar.jsx
+++ src/components/shared/Search-bar/SearchBar.jsx
@@ -6,20 +6,26 @@ import sendIcon from '/public/icons/send.svg';
 const SearchBar = () => {
   return (
     <div className="relative mb-[8px] w-[212px] sm:w-[70%] grid grid-cols-[auto_1fr_auto] items-center gap-2 border border-[#cecece] rounded-[10px] focus-within:border-primary">
-    <div className="pl-2 flex items-center">
-      <Image src={searchIcon} alt="Buscar" className="search-icon" />
-    </div>
-    <input
-      id='search-bar'
-      type='text'
-      className='py-[8px] border-none rounded-[10px] w-full focus:outline-none'
-      onFocus={(e) => e.target.previousSibling.firstChild.classList.add('invisible')}
-      onBlur={(e) => e.target.previousSibling.firstChild.classList.remove('invisible')}
-    />
-      <button className="absolute right-[-56px] top-1/2 transform -translate-y-1/2">
-        <Image src={sendIcon} alt="Enviar" />
+      <div className="pl-[10px] flex items-center">
+        <Image src={searchIcon} alt="Buscar" className="search-icon" />
+      </div>
+      <input
+        id='search-bar'
+        type='text'
+        className='pl-[20px] py-[8px] border-none rounded-[10px] w-full focus:outline-none focus:pl-0'
+        onFocus={(e) => {
+          e.target.previousSibling.classList.add('hidden');
+          e.target.classList.remove('pl-[10px]');
+        }}
+        onBlur={(e) => {
+          e.target.previousSibling.classList.remove('hidden');
+          e.target.classList.add('pl-[10px]');
+        }}
+      />
+      <button className="absolute right-[-40px] top-1/2 transform -translate-y-1/2">
+        <Image src={sendIcon} alt="Enviar" width={25} height={25} />
       </button>
-  </div>
+    </div>
   );
 };
 
