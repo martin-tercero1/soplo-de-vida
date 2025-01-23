c74c05356206b5cf50e20d8764e18dd14bd90cc2
JulianAgPerez <juli.agustin.perez@gmail.com>
2025-01-23 18:49:15 -0300

    Update SearchBar.jsx
    Now the searchbar increases its size

--- src/components/shared/Search-bar/SearchBar.jsx
+++ src/components/shared/Search-bar/SearchBar.jsx
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import searchIcon from '/public/icons/search.svg';
 import sendIcon from '/public/icons/send.svg';
 
 const SearchBar = () => {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
-    <div className="relative mb-[8px] w-[212px] sm:w-[70%] grid grid-cols-[auto_1fr_auto] items-center gap-2 border border-[#cecece] rounded-[10px] focus-within:border-primary">
-      <div className="pl-[10px] flex items-center">
-        <Image src={searchIcon} alt="Buscar" className="search-icon" />
+    <div className="relative mb-[8px] w-[212px] sm:w-[70%]">
+      <div className={`relative flex items-center border border-[#cecece] rounded-[10px] focus-within:border-primary transition-all duration-300 ease-in-out ${isFocused ? 'w-[calc(100%+38px)]' : 'w-full'}`}>
+        <div className="pl-[10px] flex items-center">
+          <Image src={searchIcon} alt="Buscar" className="search-icon" />
+        </div>
+        <input
+          id='search-bar'
+          type='text'
+          className='pl-[10px] py-[8px] border-none rounded-[10px] w-full focus:outline-none'
+          onFocus={(e) => {
+            setIsFocused(true);
+            e.target.previousSibling.classList.add('hidden');
+          }}
+          onBlur={(e) => {
+            setIsFocused(false);
+            e.target.previousSibling.classList.remove('hidden');
+          }}
+        />
       </div>
-      <input
-        id='search-bar'
-        type='text'
-        className='pl-[20px] py-[8px] border-none rounded-[10px] w-full focus:outline-none focus:pl-0'
-        onFocus={(e) => {
-          e.target.previousSibling.classList.add('hidden');
-          e.target.classList.remove('pl-[10px]');
-        }}
-        onBlur={(e) => {
-          e.target.previousSibling.classList.remove('hidden');
-          e.target.classList.add('pl-[10px]');
-        }}
-      />
-      <button className="absolute right-[-40px] top-1/2 transform -translate-y-1/2">
+      <button className={`absolute top-1/2 transform -translate-y-1/2 transition-all duration-300 ease-in-out ${isFocused ? 'right-[-94px]' : 'right-[-56px]'}`}>
         <Image src={sendIcon} alt="Enviar" width={25} height={25} />
       </button>
     </div>
